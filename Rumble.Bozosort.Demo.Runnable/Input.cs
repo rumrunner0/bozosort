5b596c79e42897587afac530dd5efb146c2b7a3a
Roman Malyi <36127049+rumrunner0@users.noreply.github.com>
2023-06-29 15:09:13 +0300

    Added summary and guard clauses to the Input class

--- Rumble.Bozosort.Demo.Runnable/Input.cs
+++ Rumble.Bozosort.Demo.Runnable/Input.cs
@@ -3,24 +3,42 @@ using System.IO;
 
 namespace Rumble.Bozosort.Demo.Runnable;
 
+/// <summary>
+/// Basic and strait input handling utility.
+/// </summary>
 internal static class Input
 {
-	internal static TParsable Line<TParsable>(TextReader source) where TParsable : IParsable<TParsable>
+	/// <summary>
+	/// Reads a line using the <see cref="TextReader"/> and parses that data to "<typeparamref name="TParsable"/>".
+	/// </summary>
+	/// <param name="reader">Reader used to read the data</param>
+	/// <typeparam name="TParsable">Type of the data</typeparam>
+	/// <returns>Data of "<typeparamref name="TParsable"/>" type</returns>
+	/// <exception cref="ApplicationException">Thrown if data can't be read or parsed to "<typeparamref name="TParsable"/>"</exception>
+	internal static TParsable Line<TParsable>(TextReader reader) where TParsable : IParsable<TParsable>
 	{
-        ArgumentNullException.ThrowIfNull(source);
+		ArgumentNullException.ThrowIfNull(reader);
 
-        var input = default(string);
-		while(string.IsNullOrEmpty(input) || string.IsNullOrWhiteSpace(input))
+		var input = default(string);
+		try
 		{
-			input = (source.ReadLine() ?? string.Empty).Trim();
+			while(string.IsNullOrWhiteSpace(input))
+			{
+				input = (reader.ReadLine() ?? string.Empty).Trim();
+			}
+		}
+		catch(Exception e)
+		{
+			throw new ApplicationException
+			(
+				$"An error occured while reading data using {typeof(TextReader)}. Details: {e.Message}",
+				innerException: e
+			);
 		}
 
 		if(TParsable.TryParse(input, provider: null, out var parsedInput) is false)
 		{
-			throw new IOException
-			(
-				$""
-			);
+			throw new ApplicationException($"Read data can't be parsed into {typeof(TParsable)}.");
 		}
 
 		return parsedInput;
